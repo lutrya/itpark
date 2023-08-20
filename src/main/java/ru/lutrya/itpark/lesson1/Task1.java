b2cc0a0455629085bc6a850b53fa9bf139017fbc
lutrya <shigoreva.vika@mail.ru>
2023-08-20 11:04:51 +0300

    Добавлено решение первого задания

--- src/main/java/ru/lutrya/itpark/lesson1/Task1.java
+++ src/main/java/ru/lutrya/itpark/lesson1/Task1.java
@@ -1,7 +1,12 @@
 package ru.lutrya.itpark.lesson1;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-
+        Scanner console = new Scanner(System.in);
+        int a = console.nextInt();
+        int b = console.nextInt();
+        System.out.println(a+b);
     }
 }
