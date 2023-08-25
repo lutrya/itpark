63109aa6f33efa664a85ad95b80897807e7140db
lutrya <shigoreva.vika@mail.ru>
2023-08-25 14:27:28 +0300

    Добавлено решение шестого задания

---
+++ src/main/java/ru/lutrya/itpark/lesson1/Task7.java
@@ -0,0 +1,12 @@
+package ru.lutrya.itpark.lesson1;
+
+import java.util.Scanner;
+
+public class Task7 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int n = console.nextInt();
+
+        System.out.println("Ответ: " + (n + 1));
+    }
+}
