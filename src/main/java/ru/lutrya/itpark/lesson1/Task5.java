42341553a89e710931333dbdb2a3c3abceac303a
lutrya <shigoreva.vika@mail.ru>
2023-08-25 13:35:16 +0300

    Добавлено решение пятого задания

---
+++ src/main/java/ru/lutrya/itpark/lesson1/Task5.java
@@ -0,0 +1,11 @@
+package ru.lutrya.itpark.lesson1;
+
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+
+        System.out.println("За этот период полных недель прошло " + 234 / 7);
+
+    }
+}
