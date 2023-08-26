0796bbf732491e999c90c47cfcf11e6d80513d09
lutrya <shigoreva.vika@mail.ru>
2023-08-26 11:57:24 +0300

    Добавлено решение десятого задания

---
+++ src/main/java/ru/lutrya/itpark/lesson1/Task10.java
@@ -0,0 +1,14 @@
+package ru.lutrya.itpark.lesson1;
+
+import java.util.Scanner;
+
+public class Task10 {
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int v = console.nextInt();
+        int m = console.nextInt();
+
+        System.out.println("Плотность материала равна: " + (m / v));
+    }
+}
