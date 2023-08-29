c9808b1acb5b31c86beff87636bbd819e6577340
lutrya <shigoreva.vika@mail.ru>
2023-08-29 09:08:06 +0300

    Добавлено решение первой задачи

---
+++ src/main/java/ru/lutrya/itpark/lesson2/Task1.java
@@ -0,0 +1,23 @@
+package ru.lutrya.itpark.lesson2;
+
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+
+        Scanner console = new Scanner(System.in);
+
+        int i = 0;
+        int sum = 0;
+
+        while (i < 10) {
+            int number = console.nextInt();
+            sum = sum + number;
+            i++;
+        }
+
+        System.out.println(sum);
+
+    }
+}
