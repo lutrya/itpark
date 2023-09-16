b923ef65ce9c8b3a2191b2bf9e43542ef8c43f4a
lutrya <shigoreva.vika@mail.ru>
2023-09-17 01:37:16 +0300

    Добавлено решение задачи 4.98

---
+++ src/main/java/ru/lutrya/itpark/lesson2/Task24.java
@@ -0,0 +1,33 @@
+package ru.lutrya.itpark.lesson2;
+
+import java.util.Scanner;
+
+public class Task24 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        double a = console.nextDouble();
+        double b = console.nextDouble();
+        double c = console.nextDouble();
+
+        double max = 0;
+        double min = 0;
+
+        if (a > b && a > c) {
+            max = a;
+        } else if (b > a && b > c) {
+            max = b;
+        } else {
+            max = c;
+        }
+        System.out.println("Максимальное значение: " + max);
+
+        if (a < b && a < c) {
+            min = a;
+        } else if (b < a && b < c) {
+            min = b;
+        } else {
+            min = c;
+        }
+        System.out.println("Минимальное значение: " + min);
+    }
+}
